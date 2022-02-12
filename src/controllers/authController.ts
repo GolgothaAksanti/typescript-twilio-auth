a2faada239db151bd2b351d8291e38410f702e58
GolgothaAksanti <goloking777@gmail.com>
2022-02-13 01:41:30 +0300

    ft(user auth) : auth and verify user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { TwilioService } from '../plugins/twilio';
-// import { generateToken } from '../utils/generateToken';
 import { User } from '../models/user';
+import { generateToken } from '../utils/generateToken';
 
 export class AuthController {
   constructor(private twilioService: TwilioService) {}
@@ -31,6 +31,85 @@ export class AuthController {
       data: {
         phonenumber: user.phonenumber,
         fullname: `${user.firstname} ${user.lastname}`,
+      },
+    });
+  };
+
+  verifyUser = async (req: Request, res: Response): Promise<Response> => {
+    const {
+      phonenumber,
+      code,
+    }: {
+      phonenumber: string;
+      code: string;
+    } = req.body;
+
+    const user = await User.findOne({ phonenumber });
+
+    if (!user) {
+      return res.status(400).json({
+        status: 400,
+        message: 'please sign up before account verification',
+      });
+    }
+
+    if (user.verified) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Your account has already verified',
+      });
+    }
+
+    const verifyCode = await this.twilioService.verifyCode(phonenumber, code);
+
+    if (!verifyCode.valid && verifyCode.status !== 'approved') {
+      return res.status(400).json({
+        status: 400,
+        message: 'wrong verification code',
+      });
+    }
+
+    await User.updateOne({ verified: true });
+    const token = generateToken(user._id);
+
+    return res.status(201).json({
+      status: 201,
+      message: 'user successfully created',
+      data: {
+        phonenumber,
+        token,
+      },
+    });
+  };
+
+  login = async (req: Request, res: Response): Promise<Response> => {
+    const { phonenumber }: { phonenumber: string } = req.body;
+
+    const user = await User.findOne({ phonenumber });
+
+    if (!user) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Please sign up before login',
+      });
+    }
+
+    if (!user.verified) {
+      await this.twilioService.sendVericationCode(phonenumber);
+      return res.status(403).json({
+        status: 403,
+        message: 'check your sms for account verification',
+      });
+    }
+
+    const token = generateToken(user._id);
+
+    return res.status(200).json({
+      status: 200,
+      message: 'success',
+      data: {
+        phonenumber,
+        token,
       },
     });
   };
